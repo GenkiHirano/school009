024786caf456daa3819fb9fe24fef91f4cfe5d9e
GenkiHirano <g.hirano@gemcook.com>
2021-11-19 18:28:18 +0900

    第9回スクール宿題

--- homework009/subject_score.go
+++ homework009/subject_score.go
@@ -6,6 +6,21 @@ import (
 
 // 第9回スクール 問題7を実装してください。
 
+// 中身を適宜変更してください。引数、メソッド、関数名も変更して大丈夫です。
+func scoreMath() {
+}
+
+// 中身を適宜変更してください。引数、メソッド、関数名も変更して大丈夫です。
+func scoreEnglish() {
+}
+
+// 中身を適宜変更してください。引数、メソッド、関数名も変更して大丈夫です。
+func scoreScience() {
+}
+
 func Task1() {
 	fmt.Println("homework009_1")
+	scoreMath()
+	scoreEnglish()
+	scoreScience()
 }
